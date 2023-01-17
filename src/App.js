f40a7147fd4fbd0df9331779e68d2ec292fae4d3
Kaushal <kk.baghel7818@gmail.com>
2023-01-17 16:27:44 +0530

    creat header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./PAGES/Home";
+import Offers from "./PAGES/Offers";
+import Signin from "./PAGES/Signin";
+import Signup from "./PAGES/Signup";
+import Forgotpassword from "./PAGES/Forgotpassword";
+import Header from "./components/Header";
 function App() {
   return (
-    <h1 className="sm:bg-red-500 md:bg-green-500">hello</h1>
+    <>
+    <Router>
+        <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/forgotpassword" element={<Forgotpassword />} />
+      </Routes>
+    </Router>
+    </>
     
   );
 }
