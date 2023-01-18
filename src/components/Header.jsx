1d61793494b50d2540243e4113db1d906804162b
Kaushal <kk.baghel7818@gmail.com>
2023-01-18 13:01:20 +0530

    complete Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useLocation,useNavigate,usenavigation } from 'react-router'
 
 export default function Header() {
+
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    function macthroute(route){
+        if(route==location.pathname){
+            return true
+        }
+    }
   return (
     <div className='bg-white border-b-2 shodow-sm sticky top-0 z-50'>
         <header className='flex justify-between items-center px-5 max-w-6xl mx-auto'>
@@ -8,14 +18,33 @@ export default function Header() {
                 <img
                 src='https://b2bweb.realtor.com/web_assets/rdc/SFMC-lp-images/2021/RDC%20-tagline-wide.png'
                 alt='Realtor Logo'
-                className='h-5'/>
+                className='h-5 cursor-pointer'
+                onClick={()=>navigate('/')}/>
             </div>
             <div>
-                <ul className='flex space-x-6'>
-                    <li className='py-3 text-slate-500 text-sm font-semibold'>Home </li>
-                    <li className='py-3 text-slate-500 text-sm font-semibold'>Offers</li>
-                    <li className='py-3 text-slate-500 text-sm font-semibold'>Sign In</li>
-                </ul>
+                <ul className='flex space-x-8'>
+                    <li className={`cursor pointer py-3 text-slate-500 text-sm font-semibold border-b-[3px]
+                          border-b-transparent ${macthroute('/') && 'text-black border-b-red-500'}
+                        `}
+                         onClick={()=>navigate('/')}
+                    >
+                    Home
+                    </li>
+                    <li className={`cursor pointer py-3 text-slate-500 text-sm font-semibold border-b-[3px]
+                          border-b-transparent ${macthroute('/offers') && 'text-black border-b-red-500'}
+                        `}
+                         onClick={()=>navigate('/offers')}
+                    >
+                    Offers
+                    </li>
+                    <li className={`cursor pointer py-3 text-slate-500 text-sm font-semibold border-b-[3px]
+                          border-b-transparent ${macthroute('/sign-in') && 'text-black border-b-red-500'}
+                        `}
+                         onClick={()=>navigate('/sign-in')}
+                    >
+                    Sign-in
+                    </li>
+                    </ul>
 
             </div>
         </header>
